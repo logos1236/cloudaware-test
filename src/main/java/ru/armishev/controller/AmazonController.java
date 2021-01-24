ff38480335addb59bb885b6ae9d61e1ec17c6674
logos <armishev@vigroup.ru>
2021-01-24 23:53:02 +0500

    Custimization downloader scheduling

--- src/main/java/ru/armishev/controller/AmazonController.java
+++ src/main/java/ru/armishev/controller/AmazonController.java
@@ -14,27 +14,14 @@ import java.util.*;
 @RequestMapping(value="/api/v1/list")
 public class AmazonController {
     private final AmazonObjectJPA amazonObjectJPA;
-    private final IAmazonEntity amazonEntity;
 
     @Autowired
-    public AmazonController(AmazonObjectJPA amazonObjectJPA, IAmazonEntity amazonService) {
+    public AmazonController(AmazonObjectJPA amazonObjectJPA) {
         this.amazonObjectJPA = amazonObjectJPA;
-        this.amazonEntity = amazonService;
     }
 
     @GetMapping("")
     public List<AmazonObjectEntity> getList() {
         return amazonObjectJPA.findAll();
-    }
-
-    @GetMapping("/add")
-    public List<AmazonObjectEntity> addElement() {
-        List<AmazonObjectEntity> listForSave = amazonEntity.getList();
-
-        if (!listForSave.isEmpty()) {
-            amazonObjectJPA.saveAll(listForSave);
-        }
-
-        return listForSave;
     }
 }
--- src/main/java/ru/armishev/scheduler/AmazonDownloadScheduler.java
+++ src/main/java/ru/armishev/scheduler/AmazonDownloadScheduler.java
@@ -1,12 +1,33 @@
 package ru.armishev.scheduler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import ru.armishev.entity.AmazonObjectEntity;
+import ru.armishev.jpa.AmazonObjectJPA;
+import ru.armishev.service.IAmazonEntity;
+
+import java.util.List;
 
 @Component
 public class AmazonDownloadScheduler {
+    private final AmazonObjectJPA amazonObjectJPA;
+    private final IAmazonEntity amazonEntity;
+
+    @Autowired
+    public AmazonDownloadScheduler(AmazonObjectJPA amazonObjectJPA, IAmazonEntity amazonEntity) {
+        this.amazonObjectJPA = amazonObjectJPA;
+        this.amazonEntity = amazonEntity;
+    }
+
     @Scheduled(fixedDelay = 5000)
     public void myScheduler() {
-        System.out.println("Test print");
+        List<AmazonObjectEntity> listForSave = amazonEntity.getList();
+
+        if (!listForSave.isEmpty()) {
+            amazonObjectJPA.saveAll(listForSave);
+        }
+
+        System.out.println("Download");
     }
 }
