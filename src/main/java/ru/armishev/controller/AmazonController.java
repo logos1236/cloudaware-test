b7648c07dfe5b3727b40d1cd351d9c7ffc177852
logos <armishev@vigroup.ru>
2021-01-25 17:31:11 +0500

    Add paginator

--- src/main/java/ru/armishev/controller/AmazonController.java
+++ src/main/java/ru/armishev/controller/AmazonController.java
@@ -1,10 +1,13 @@
 package ru.armishev.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.armishev.entity.AmazonObjectEntity;
 import ru.armishev.jpa.AmazonObjectJPA;
 
@@ -21,8 +24,11 @@ public class AmazonController {
     }
 
     @GetMapping("")
-    public String getList(Model model) {
-        model.addAttribute("listS3", amazonObjectJPA.findAll());
+    public String getList(Model model, @RequestParam(name = "page", required = false) Integer page) {
+        page = (page == null) ? 0 : page;
+        Pageable firstPageWithTwoElements = PageRequest.of(page, 1);
+
+        model.addAttribute("listS3", amazonObjectJPA.findAll(firstPageWithTwoElements));
 
         return "list";
     }
