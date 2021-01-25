8d98180fec62c52f79e610e074b582bd3a365fd6
logos <armishev@vigroup.ru>
2021-01-25 16:40:36 +0500

    html view

--- src/main/java/ru/armishev/controller/AmazonControllerRest.java
+++ src/main/java/ru/armishev/controller/AmazonControllerRest.java
@@ -1,29 +1,27 @@
 package ru.armishev.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.armishev.entity.AmazonObjectEntity;
 import ru.armishev.jpa.AmazonObjectJPA;
+import ru.armishev.service.IAmazonEntity;
 
-import java.util.List;
+import java.util.*;
 
-@Controller
-@RequestMapping(value="/list")
-public class AmazonController {
+@RestController
+@RequestMapping(value="/api/v1/list")
+public class AmazonControllerRest {
     private final AmazonObjectJPA amazonObjectJPA;
 
     @Autowired
-    public AmazonController(AmazonObjectJPA amazonObjectJPA) {
+    public AmazonControllerRest(AmazonObjectJPA amazonObjectJPA) {
         this.amazonObjectJPA = amazonObjectJPA;
     }
 
     @GetMapping("")
-    public String getList(Model model) {
-        model.addAttribute("listS3", amazonObjectJPA.findAll());
-
-        return "list";
+    public List<AmazonObjectEntity> getList() {
+        return amazonObjectJPA.findAll();
     }
 }
